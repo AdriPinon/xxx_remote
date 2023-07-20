292473417abd782f81e1f07c950d858838092b55
Adriana Pinon <adrianapinon000@gmail.com>
2023-07-20 12:36:00 -0700

    Initial Commit

---
+++ xxx/xxxApp.swift
@@ -0,0 +1,17 @@
+//
+//  xxxApp.swift
+//  xxx
+//
+//  Created by Adriana Pinon on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct xxxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
