497951e29c1c4e1869f46d37334e14094d3a6454
Adriana Pinon <adrianapinon000@gmail.com>
2023-07-20 15:44:52 -0700

    added new txt(kms)

--- xxx/ContentView.swift
+++ xxx/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("RAWRRRRRRRRR")
+            Text("KMS")
         }
         .padding()
     }
